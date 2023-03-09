25edd29ec336f0a8444f081336035768c6efd405
Huseyin Uysal <huseyin_uysal97@hotmail.com>
2023-03-09 21:32:02 +0300

    install next js and create the first template

---
+++ src/app/head.jsx
@@ -0,0 +1,11 @@
+export default function Head(){
+    return(
+        <>
+        <title>BAHAR</title>
+        <meta content="width=device-width, initial-scale=1"
+        name="viewport" />
+        <meta name="description" content="Bahar created by Uysal" />
+        <link rel="icon" href="/favicon.ico" />  
+        </>
+    )
+}
