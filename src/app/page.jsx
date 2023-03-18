054ded2357dbbbc347904d358956c9a8cfb12386
Huseyin Uysal <huseyin_uysal97@hotmail.com>
2023-03-18 13:30:08 +0300

    add body section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import HomeHeader from '@/components/HomeHeader'
+import HomeSearch from '@/components/HomeSearch'
+import Image from 'next/image'
+import myImg from './baharlogo.jpg'
 
 export default function Home() {
   return (
     <>
     <HomeHeader/>
+
+
+    <div className="flex flex-col items-center mt-24">
+      <Image
+      src={myImg}
+      width="300"
+      height="100"
+      alt="bahar logo"
+      />
+      <HomeSearch/>
+
+    </div>
     </>
   )
 }
